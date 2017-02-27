3f9fa784d8d93362af3b2ecb5cbc4e537404df27
ghamzak <ghka9436@colorado.edu>
2017-02-27 09:39:35 -0700

    third commit: modified templates to display results of the query from db.

--- corpusbank/views.py
+++ corpusbank/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Corpusobject
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'corpusbank/post_list.html', {})
+    desired = Corpusobject.objects.filter(knownloc = True).order_by('catid')
+    return render(request, 'corpusbank/post_list.html', {'desired' : desired})
