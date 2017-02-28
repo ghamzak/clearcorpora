6bebb94034728b24e68709f0b6a9d955865040b6
ghamzak <ghka9436@colorado.edu>
2017-02-28 14:19:04 -0700

    added forms for search, changed the base template to contain a link to the search page, added new templates for search form and search results pages, changed the urls file accordingly, changed the views to contain functions to search the database and return desired results based on the queries

--- corpusbank/forms.py
+++ corpusbank/forms.py
@@ -7,3 +7,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Corpusobject
         fields = ('name', 'catid', 'verbsaddress', 'language', 'project', 'knownloc', 'physical', 'downloadable', 'project')
+
+
+# class SearchForm(forms.Form):
+# 	q_name = forms.CharField(label='Corpus Name', max_length=30)
+# 	q_variants_sku_cont = forms.CharField(label='Catalog Number', max_length=30)
+# 	
--- corpusbank/views.py
+++ corpusbank/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Corpusobject
 from .forms import PostForm
+import operator
+from django.db.models import Q
+from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request):
@@ -36,5 +39,36 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'corpusbank/post_edit.html', {'form': form})
 
+
+def search_form(request):
+    return render(request, 'corpusbank/search_form.html')
+
+def search(request):
+	error = False
+	if 'q[name]' in request.GET:
+		qn = request.GET['q[name]']
+		qc = request.GET['q[catid]']
+		ql = request.GET['q[languages][]']
+		if not (qn or qc or ql):
+			error = True
+		elif qn and (not qc and not ql):
+			myresults = Corpusobject.objects.filter(name__icontains=qn)
+			return render(request, 'corpusbank/search_results.html', {'myresults': myresults, 'query': qn})
+		elif qc and (not qn and not ql):
+			myresults = Corpusobject.objects.filter(catid__icontains=qc)
+			return render(request, 'corpusbank/search_results.html', {'myresults': myresults, 'query': qc})
+		elif ql and (not qn and not qc):
+			myresults = Corpusobject.objects.filter(language__icontains=ql)
+			return render(request, 'corpusbank/search_results.html', {'myresults': myresults, 'query': ql})
+		elif (qc and qn) and not ql:
+			myresults = Corpusobject.objects.filter(name__icontains=qn).filter(catid__icontains=qc)
+			return render(request, 'corpusbank/search_results.html', {'myresults': myresults})
+		elif (qc and ql) and not qn:
+			myresults = Corpusobject.objects.filter(catid__icontains=qc).filter(language__icontains=ql)
+			return render(request, 'corpusbank/search_results.html', {'myresults': myresults})
+		elif (qn and ql) and not qc:
+			myresults = Corpusobject.objects.filter(name__icontains=qn).filter(language__icontains=ql)
+			return render(request, 'corpusbank/search_results.html', {'myresults': myresults})
+	return render(request, 'corpusbank/search_form.html', {'error': error})
 
 
