56a50c33aa7d939a79adf3eb3b7300e9a08f950b
ghamzak <ghka9436@colorado.edu>
2017-02-27 20:18:54 -0700

    modified css, defined a base template, added post details, modified urls and views

--- corpusbank/views.py
+++ corpusbank/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Corpusobject
 
 # Create your views here.
 def post_list(request):
-    desired = Corpusobject.objects.filter(knownloc = True).order_by('catid')
-    return render(request, 'corpusbank/post_list.html', {'desired' : desired})
+    posts = Corpusobject.objects.filter(knownloc = True).order_by('catid')
+    return render(request, 'corpusbank/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Corpusobject, pk=pk)
+	return render(request, 'corpusbank/post_detail.html', {'post': post})
